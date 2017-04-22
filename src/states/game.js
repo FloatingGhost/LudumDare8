6f021a7d1d318f813a997232f60a875772623a30
Hannah Ward <Hannah.ward9001@gmail.com>
2017-04-22 09:34:47 +0100

    new: boat faces direction of travel

--- src/states/game.js
+++ src/states/game.js
@@ -5,62 +5,93 @@ Game.prototype = {
   towers: null,
   notes: null,
   volText: null,
+  select: null,
+  river: null,
 
   init: function() {
-    console.log(this);
     setCurrency(100);
   },
 
   preload: function() {
-    game.load.image("bg", "../res/img/game-placeholder-bg.png");
+    game.load.image("river", "../res/img/game-placeholder-bg.png");
     game.load.image("boat", "../res/img/Boat.png");
     game.load.image("tower", "../res/img/Tower.png");
     game.load.image("gun", "../res/img/Gun.png");
     game.load.image("note", "../res/img/Note.png");
     game.load.image("rider", "../res/img/Rider.png");
     game.load.image("AOE", "../res/img/AOE.png");
+    game.load.image("Select", "../res/img/Select.png");
+    game.load.image("bg", "../res/img/BG.png");
   },
 
   create: function() {
     game.add.sprite(0,0,"bg");
+    this.river = game.add.sprite(0,0,"river");
+    this.river.inputEnabled = true;
+    this.river.input.pixelPerfectClick = true;
     this.boats = game.add.group();
     this.towers = game.add.group();
     this.notes = game.add.group();
     this.spawnBoat();
-    game.input.onDown.add(this.spawnTower.bind(this, this.towers));
+    game.input.onDown.add(()=>{
+      this.select.x = -100;
+      // Check for if we've clicked a previously exisiting node
+      var clickedOn = this.towers.filter((tower) => (
+        getAbsoluteDistance(game.input.activePointer, tower) < tower.width/2
+      ));
+      // If not, spawn
+      if (clickedOn.first) {
+        this.select.x = clickedOn.first.x;
+        this.select.y = clickedOn.first.y;
+        this.select.selected = clickedOn.first;
+      } else {
+        if (!this.river.input.pointerOver()) {
+          this.spawnTower(this.towers);
+        }
+      }
+    });
     this.volText = game.add.text(0,0,"Volume at mouse: 0 dB");
+    this.select = game.add.sprite(-100, -100, "Select");
+    this.select.anchor.setTo(0.5, 0.5);
   },
 
   update: function() {
     // Get the volume at the current mouse position
     
-    var vol = this.getVolAtMouse();
+    var vol = this.getVolAt(game.input.activePointer);
     this.volText.text = "Volume at mouse: "+vol+" dB"
+
+    this.boats.forEach((boat) => {
+      boat.rotation = Phaser.Math.angleBetweenPoints(
+                        new Phaser.Point(0,0),
+                        new Phaser.Point(boat.deltaX, boat.deltaY))
+    });
   },
 
-  getVolAtMouse: function() {
+  getVolAt: function(point) {
     // First, get all nodes in range
     // So that'll be distance <= 100*range
     var inRange = this.towers.filter(
-      (tower) => (getAbsoluteDistance(game.input.activePointer,
+      (tower) => (getAbsoluteDistance(point,
                                       tower) <= 100*tower.range));
     var s = 0;
-    console.log(inRange);
     inRange.list.forEach((i)=>(s+=i.power));
     return s;
   },
 
   spawnBoat: function() {
-    var thisBoat = this.boats.create(700, 350, "boat");
+    var thisBoat = this.boats.create(850, 400, "boat");
+    thisBoat.anchor.setTo(0.5,0.5);
     // Start the boat off on its magical journey to the asylum
     var t = game.add.tween(thisBoat).to( 
-      {x: Levels.coords[0].map((x)=>(x.x - 100)),
-       y: Levels.coords[0].map((x)=>(x.y - 50))},
+      {x: Levels.coords[0].map((x)=>(x.x)),
+       y: Levels.coords[0].map((x)=>(x.y))},
       16000,
       "Linear"
     );
     t.interpolation(Phaser.Math.catmullRomInterpolation);
     t.onComplete.add(this.killBoat);
+    thisBoat.tweenie = t;
     t.start();
   },
 
