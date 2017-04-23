babc6d1bcd846760f36b09febfce32f22ed7c519
Hannah Ward <Hannah.ward9001@gmail.com>
2017-04-23 04:30:37 +0100

    UI dissapears now

--- src/behaviours.js
+++ src/behaviours.js
@@ -13,6 +13,8 @@ var waveBehaviour = function(waveType, amp, freq, offset) {
   this.amp = amp;
   this.freq = freq;
   this.offset = offset;
+  this.damageMultiplier = 1;
+  
   // Game balance - intermittent speakers are cheaper
   switch (this.waveType) {
     case waves.CONSTANT:
@@ -31,19 +33,68 @@ waveBehaviour.prototype = {
   output:  0,
   powerUse: 0,
   zeroTicks: 0,
+  rangeModifier: 0,
+  isOff: false,
+
+  canPowerUp: function() {
+    // Have a cap on damage multipliers 
+    return this.damageMultiplier <= 4;    
+  },
+
+  powerUp: function() {
+    // increase damage
+    this.damageMultiplier += 1;
+  },
+
+  canRangeUp: function() {
+    return this.rangeModifier <=2;
+  },
+
+  rangeUp: function() {
+    this.rangeModifier += 1;
+  },
+
+  togglePower: function() {
+    this.isOff = !this.isOff;
+  },
+
+  incrementFreq: function() {
+    this.freq = (this.freq + 1)%10;
+    if (this.freq == 0) this.freq++;
+  },
+
+  decrementFreq: function() {
+    this.freq--;
+    if (this.freq == 0) this.freq = 9;
+  },
+
+  incrementOff: function() {
+    this.offset = (this.offset + 1)%10;
+    if (this.offset == 0) this.offset++;
+  },                        
+                            
+  decrementOff: function() {
+    this.offset--;
+    if (this.offset == 0) this.offset = 9;
+  },
 
   update: function() {
     // On tick, update waveform
     if (this.zeroTicks) {
-      this.powerUse = 0;
       this.output = 0;
       this.zeroTicks--;
       return;    
     }
+    if (this.isOff) {
+      this.powerUse = 0;
+      this.output = 0;
+      this.curTime = 0;
+      return;
+    }
 
     this.curTime += 0.1;
-    this.output = this.waveType(this.amp, this.freq, 
-                                this.offset, this.curTime);
+    this.output = this.damageMultiplier * this.waveType(this.amp, this.freq, 
+                                                  this.offset, this.curTime);
     // We don't ever want to heal the riders' sanity
     if (this.output < 0) this.output = 0;
     // Power use should be exponential to stop users just 
@@ -73,8 +124,8 @@ if (!Array.prototype.sum){
 
 var getAbsoluteDistance = function(s1, s2) {
   return Math.sqrt(
-            Math.pow(Math.abs(s1.x - s2.x), 2) + 
-            Math.pow(Math.abs(s1.y - s2.y), 2));
+            Math.pow(Math.abs(s1.centerX - s2.x), 2) + 
+            Math.pow(Math.abs(s1.centerY - s2.y), 2));
 };
 
 var filterByDistance = function(tower, boatGroup, maxRange) {
@@ -83,7 +134,7 @@ var filterByDistance = function(tower, boatGroup, maxRange) {
 };
 
 var shootFirst = function(tower, boatGroup) {
-  var dist = filterByDistance(tower, boatGroup, 500);
+  var dist = filterByDistance(tower, boatGroup, 110*(1+tower.wave.rangeModifier));
   return dist.first;
 };
 
--- src/states/game.js
+++ src/states/game.js
@@ -14,6 +14,19 @@ Game.prototype = {
   constantButton: null,
   buttonSelected: null,
   towerToPlace: -1,
+  sinUI: null,
+  // Upgrades
+  rangeUp: null,
+  powerUp: null,
+  togglePow: null,
+  surgeIcon: null,
+  freqInc: null,
+  freqDec: null,
+  offInc: null,
+  offDec: null,
+
+  // Ui shading
+  buttonBar: null,
 
   init: function() {
     setCurrency(100);
@@ -32,48 +45,150 @@ Game.prototype = {
     game.load.image("sineButton", "../res/img/SineButton.png");
     game.load.image("constantButton", "../res/img/ConstantButton.png");
     game.load.image("buttonSelect", "../res/img/ButtonSelect.png");
+    game.load.image("powerUp", "../res/img/PowerUp.png");
+    game.load.image("rangeUp", "../res/img/RangeUp.png");
+    game.load.image("togglePow", "../res/img/TogglePower.png");
+    game.load.image("powerSymbol", "../res/img/PowerSymbol.png");
+    game.load.image("speakerSymbol", "../res/img/SpeakerSmybol.png");
+    game.load.image("surgeIcon", "../res/img/SurgeIcon.png");
+    game.load.image("powerIcon", "../res/img/OffIcon.png");
+    game.load.image("increment", "../res/img/Increment.png");
+    game.load.image("decrement", "../res/img/Decrement.png");
   },
 
   create: function() {
+    // Start physics engine for collision detection
+    game.physics.startSystem(Phaser.Physics.P2JS);
+  
+    // Add backgrounds
     game.add.sprite(0,0,"bg");
     this.river = game.add.sprite(0,0,"river");
     this.river.inputEnabled = true;
     this.river.input.pixelPerfectClick = true;
+    
+    // Add a group to keep track of the various sprites
     this.boats = game.add.group();
     this.towers = game.add.group();
     this.notes = game.add.group();
+
+    // Add UI bar to make user know they shouldn't place a tower there
+    this.buttonBar = game.add.graphics();
+    this.buttonBar.alpha = 0.3;
+    this.buttonBar.beginFill(0x000000);
+    this.buttonBar.drawRect(0, 540, 800, 60);
+    this.buttonBar.endFill();
+
+    // TEST: Make a boat to make sure it works
     this.spawnBoat();
+
+    // Bind click check
+    // Is context-sensitive
     game.input.onDown.add(()=>{
-      this.select.x = -100;
-      // Check for if we've clicked a previously exisiting node
-      var clickedOn = this.towers.filter((tower) => (
-        getAbsoluteDistance(game.input.activePointer, tower) < tower.width/2
-      ));
-      // If not, spawn
-      if (clickedOn.first) {
-        this.select.x = clickedOn.first.x;
-        this.select.y = clickedOn.first.y;
-        this.select.selected = clickedOn.first;
+      // Two seperate checks
+      // If we've clicked in the button bar, process as a button press
+      // Else, do tower placement checks
+      if (game.input.activePointer.y > 540) {
+        // PROCESS AS A BUTTON
+        
+        if (this.constantButton.input.pointerOver()) {
+          // Selected a CONSTANT tower to place  
+          this.buttonSelected.x = this.constantButton.x;
+          this.buttonSelected.y = this.constantButton.y;
+          this.towerToPlace = 1;
+          
+        } else if (this.sineButton.input.pointerOver()) {
+          // Selected a SINE tower to place
+          this.buttonSelected.x = this.sineButton.x;
+          this.buttonSelected.y = this.sineButton.y;
+          this.towerToPlace = 2;
+        
+        } else if (this.select.selected) {
+          // Upgrade UI is visible. check for clicks
+          
+          if (this.togglePow.input.pointerOver()) {
+            this.select.selected.wave.togglePower();
+            this.select.selected.powerIcon.alpha ^= 1;
+          } else if (this.powerUp.input.pointerOver()) {
+            if (this.select.selected.wave.canPowerUp()) {
+              this.select.selected.wave.powerUp();
+            }
+          } else if (this.rangeUp.input.pointerOver()) {
+            if (this.select.selected.wave.canRangeUp()) {
+              this.select.selected.wave.rangeUp();
+              this.calculateArc(this.select.selected);
+            }
+          }
+
+          if (this.freqDec.alpha) {
+            // Sine UI is visible
+        
+            if (this.freqInc.input.pointerOver()) {
+              this.select.selected.wave.incrementFreq();
+              this.freqInc.label.text = this.select.selected.wave.freq;       
+            } else if (this.freqDec.input.pointerOver()) {
+              this.select.selected.wave.decrementFreq();
+              this.freqInc.label.text = this.select.selected.wave.freq
+            } else if (this.offInc.input.pointerOver()) {
+              this.select.selected.wave.incrementOff();
+              this.offInc.label.text = this.select.selected.wave.offset;
+            } else if (this.offDec.input.pointerOver()) {
+              this.select.selected.wave.decrementOff();
+              this.offInc.label.text = this.select.selected.wave.offset;
+            }
+          } 
+        }
+        
       } else {
-        if (!this.river.input.pointerOver()) {
-          // Butt
-          if (this.constantButton.input.pointerOver()) {
-            this.buttonSelected.x = this.constantButton.x;
-            this.buttonSelected.y = this.constantButton.y;
-            this.towerToPlace = 1;
-          } else if (this.sineButton.input.pointerOver()) {
-            this.buttonSelected.x = this.sineButton.x;
-            this.buttonSelected.y = this.sineButton.y;
-            this.towerToPlace = 2;
-          } else {
-            if (this.towerToPlace != -1)         
+        // PROCESS AS A TOWER PLACE/CLICK
+      
+        // Check for if we've clicked a previously exisiting node
+        var clickedOn = this.towers.filter((tower) => (
+          tower.input.pointerOver()));
+      
+        if (clickedOn.first) {
+          // We've selected a tower 
+          // Highlight it
+          this.select.x = clickedOn.first.x;
+          this.select.y = clickedOn.first.y;
+          this.select.selected = clickedOn.first;
+        
+          // Now show the upgrade UI
+          this.powerUp.alpha = this.togglePow.alpha = this.rangeUp.alpha = 1;
+      
+          // If sin, show sine UI
+          if (clickedOn.first.wave.waveType == waves.SINE) {
+            this.offDec.alpha = this.offInc.alpha = 1;
+            this.freqDec.alpha = this.freqInc.alpha = 1
+            this.offInc.label.alpha = this.freqInc.label.alpha = 1;
+
+            this.offInc.label.text = clickedOn.first.wave.offset;
+            this.freqInc.label.text = clickedOn.first.wave.freq;
+          }
+        } else {
+          // Empty space
+          // Deselect any tower
+          this.select.selected = null;
+          this.select.x=-100;
+          this.powerUp.alpha = this.togglePow.alpha = this.rangeUp.alpha = 0;
+          this.offInc.label.alpha = this.freqInc.label.alpha = 0;
+          this.offDec.alpha = this.offInc.alpha = 0;
+          this.freqDec.alpha = this.freqInc.alpha = 0; 
+
+          // Check for spawning conditions
+          if (!this.river.input.pointerOver()) {
+            // Check that we've selected a tower to place
+            if (this.towerToPlace != -1) {
+              // All spawning conditions passed
               this.spawnTower(this.towers);
+              this.towerToPlace = -1;
+              this.buttonSelected.x=-100;
+            }
           }
         }
       }
     });
-    this.volText = game.add.text(0,0,"Volume at mouse: 0 dB");
-    this.powerUse = game.add.text(0, 550, "Power Usage: 0 w");
+    this.volText = game.add.text(50,550,"Volume at mouse: 0 dB");
+    this.powerUse = game.add.text(250, 550, "Power Usage: 0 w");
     this.select = game.add.sprite(-100, -100, "Select");
     this.select.anchor.setTo(0.5, 0.5);
 
@@ -83,18 +198,54 @@ Game.prototype = {
     this.constantButton.inputEnabled = true;
     this.sineButton.inputEnabled = true;
     this.buttonSelected = game.add.sprite(-100, -100, "buttonSelect");
+
+    // Add icons
+    game.add.sprite(200, 550, "powerSymbol");
+    game.add.sprite(0, 550, "speakerSymbol");
+    this.surgeIcon = game.add.sprite(200, 500, "surgeIcon");
+    this.surgeIcon.alpha = 0;
+
+    // Add upgrade UI
+    this.powerUp = game.add.sprite(450, 550, "powerUp");
+    this.togglePow = game.add.sprite(350, 550, "togglePow");
+    this.rangeUp = game.add.sprite(400, 550, "rangeUp");
+
+    // And sine UI
+    this.offInc = game.add.sprite(550, 550, "increment");
+    this.offInc.label = game.add.text(520, 550, "0");
+    this.offDec = game.add.sprite(550, 570, "decrement");
+    this.freqInc = game.add.sprite(650, 550, "increment");
+    this.freqInc.label = game.add.text(620, 550, "0");
+    this.freqDec = game.add.sprite(650, 570, "decrement");     
+    // Make them initially invisible
+    this.powerUp.alpha = this.togglePow.alpha = this.rangeUp.alpha = 0;
+    this.offInc.alpha = this.offDec.alpha = 0;
+    this.freqDec.alpha = this.freqInc.alpha = 0;
+    this.freqInc.label.alpha = this.offInc.label.alpha = 0;
+ 
+    // Bind upgrade events
+    this.powerUp.inputEnabled = true;
+    this.togglePow.inputEnabled = true;
+    this.rangeUp.inputEnabled = true;
+    this.offInc.inputEnabled = true;
+    this.offDec.inputEnabled = true;
+    this.freqInc.inputEnabled = true;
+    this.freqDec.inputEnabled = true;
   },
 
   update: function() {
     // Get the volume at the current mouse position
     var vol = this.getVolAt(game.input.activePointer);
-    this.volText.text = "Volume at mouse: "+Number(vol).toFixed(2)+" dB"
+    this.volText.text = Number(vol).toFixed(2)+" dB"
     var power = this.towers.children.map((tower)=>(tower.wave.powerUse)).sum()
-    this.powerUse.text = "Power Usage: "+Number(power).toFixed(2)+" w";
+    this.powerUse.text = Number(power).toFixed(2)+" w";
   
     // Check for power surge
     if (power > 5) {
       this.towers.forEach((t)=>{t.wave.resetWave()});
+      this.surgeIcon.alpha = 1;
+    } else {
+      this.surgeIcon.alpha = 0;
     }
 
     this.boats.forEachAlive((boat) => {
@@ -103,30 +254,54 @@ Game.prototype = {
                         new Phaser.Point(boat.deltaX, boat.deltaY))
 
       // Deal damage
-      boat.health -= this.getVolAt(boat);
-      boat.healthIndicator.clear();
-      boat.healthIndicator.beginFill(0xFF0000);
-      boat.healthIndicator.drawRect(boat.x-50, boat.y-50, 100, 20);
-      boat.healthIndicator.beginFill(0x00FF00);
-      boat.healthIndicator.drawRect(boat.x-49, boat.y-49, boat.health, 18);
-      boat.healthIndicator.endFill();
+      boat.health -= this.getVolAt(new Phaser.Point(boat.centerX, boat.centerY));
+      if (boat.health <= 0) {
+        this.killBoat(boat);
+      }
+      if (boat.health > 0) {
+        boat.healthIndicator.clear();
+        boat.healthIndicator.beginFill(0xFF0000);
+        boat.healthIndicator.drawRect(boat.x-50, boat.y-50, 100, 20);
+        boat.healthIndicator.beginFill(0x00FF00);
+        boat.healthIndicator.drawRect(boat.x-49, boat.y-49, boat.health, 18);
+        boat.healthIndicator.endFill();
+      }
     });
 
     this.towers.forEach((tower) => {
       tower.wave.update();
-      tower.towerAOE.alpha = tower.wave.output / 10;
+      // Draw AOE
+      tower.AOE.alpha = tower.wave.output / 10;  
+      // Rotate the AOE to face the target
+      var targeting = tower.targetingBehaviour(tower, this.boats);
+      if (targeting) {
+        tower.AOE.clear();
+        tower.AOE.beginFill(0xFF0000);
+        var angleToTarget = Phaser.Math.angleBetweenPoints(tower, targeting);
+        tower.AOE.rotation = angleToTarget;
+        tower.body.rotation = angleToTarget;
+        tower.body.offset = new Phaser.Point(-40*Math.cos(-angleToTarget),
+                                             -40*Math.sin(angleToTarget));
+        tower.body.x = tower.targetX+ 40*Math.cos(-angleToTarget) - 40
+        tower.body.y = tower.targetY+ 40*Math.sin(angleToTarget)
+        tower.AOE.arc(0,0,100*(1+tower.wave.rangeModifier),
+                      Phaser.Math.degToRad(
+                        angleToTarget - (tower.angleOfEffect / 2)),
+                      Phaser.Math.degToRad(
+                        angleToTarget + (tower.angleOfEffect / 2)));
+        tower.AOE.endFill();
+      } 
     });
 
   },
 
   getVolAt: function(point) {
-    // First, get all nodes in range
-    // So that'll be distance <= 100*range
-    var inRange = this.towers.filter(
-      (tower) => (getAbsoluteDistance(point,
-                                      tower) <= 100*tower.range));
     var s = 0;
-    inRange.list.forEach((i)=>(s+=i.wave.output));
+    this.towers.forEach((tower) => {
+      if (game.physics.p2.hitTest(point, [tower]).length) {
+        s += tower.wave.output;
+      }
+    })
     return s;
   },
 
@@ -145,7 +320,7 @@ Game.prototype = {
     thisBoat.tweenie = t;
     thisBoat.health = 100;
     thisBoat.healthIndicator = game.add.graphics(0,0);
-    
+   
     t.start();
   },
 
@@ -153,15 +328,23 @@ Game.prototype = {
     console.log("BOAT DYING");
     boat.kill();
     boat.healthIndicator.clear();
+    if (boat.health <= 0) {
+      modifyCurrency(10);
+    }
   },
 
   spawnTower: function(tGroup) {
-    var thisTow = tGroup.create(game.input.activePointer.x, 
-                                game.input.activePointer.y, 
-                                "tower");
-    thisTow.anchor.setTo(0.5, 0.5);
+    var sX = game.input.activePointer.x;
+    var sY = game.input.activePointer.y;
+    var thisTow = tGroup.create(sX, sY,"tower");
     thisTow.range = 1;
     thisTow.power = 1;
+    thisTow.AOE = game.add.graphics(sX, sY);
+    thisTow.targetingBehaviour = shootFirst;
+    thisTow.angleOfEffect = 180;
+    thisTow.powerIcon = game.add.sprite(sX, sY, "powerIcon")
+    thisTow.powerIcon.alpha = 0;
+
     switch (this.towerToPlace) {
       case 1:
         thisTow.wave  = new waveBehaviour(waves.CONSTANT, 1);
@@ -170,10 +353,38 @@ Game.prototype = {
         thisTow.wave  = new waveBehaviour(waves.SINE, 1, 1, 0);
         break;
     }
-    thisTow.towerAOE = game.add.sprite(game.input.activePointer.x,
-                                       game.input.activePointer.y, "AOE")
-    thisTow.towerAOE.alpha = 0.1;
-    thisTow.towerAOE.anchor.setTo(0.5, 0.5);
-    thisTow.towerAOE.scale.setTo(2,2);
+    
+    game.physics.p2.enable(thisTow, true);
+    thisTow.body.static = true;
+    thisTow.targetX = sX + thisTow.width;
+    thisTow.targetY = sY;
+    thisTow.anchor.setTo(0.5, 0.5); 
+    thisTow.inputEnabled = true;
+    thisTow.pixelPerfectClick = true;
+    this.calculateArc(thisTow)
+  },
+
+  calculateArc: function(thisTow) {
+    var sX = thisTow.targetX;
+    var sY = thisTow.targetY;
+
+    var points = [[sX, sY]];
+    var aoeRad = Phaser.Math.degToRad(thisTow.angleOfEffect);
+    var absDist = 100*(thisTow.wave.rangeModifier + 1);
+
+    thisTow.body.clearShapes();
+    
+    thisTow.body.offset = new Phaser.Point(-thisTow.width*(thisTow.wave.rangeModifier+1), 0)
+
+    for (var i = -aoeRad/2; i <= aoeRad/2; i += 0.01) {
+      // Rotation must start at 0 for the sprite, so we can ignore it!
+      points.push([
+        sX+(absDist * Math.sin(i + (Math.PI/2))),
+        sY+(absDist * Math.cos(i + (Math.PI/2)))
+      ]);
+    }
+    thisTow.body.addPolygon({}, points);
+    thisTow.body.x = thisTow.targetX + (thisTow.width * thisTow.wave.rangeModifier);
+    thisTow.body.y = thisTow.targetY;
   }
 }
